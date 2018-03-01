b1376a67ed80bc9c628589382bbd0de1f15ace58
labrick <yananback@gmail.com>
2018-03-01 13:35:53 +0800

    添加新设备，保留之前设备的别称，格式化json文件

--- common.py
+++ common.py
@@ -15,10 +15,32 @@ def getDeviceName():
     codeList = json.loads(allText)
     return codeList
 
-#----------------------------------------------------------------------------
+# deviceDict is dict
+# each element is a list
+# each member of the list is a nickname of deviceName
+# e.g. "晓spark": ["晓", "spark"]
 def saveDeviceName(deviceDict):
+    originDeviceDict = getDeviceName()
+    for deviceName in deviceDict:
+        if deviceName in originDeviceDict:
+            continue
+        else:
+            print("new device " + deviceName + ", please add the nickname of the device")
+            originDeviceDict[deviceName] = []
+
     filep = open("deviceNickname.json", 'w')
-    js = json.dumps(deviceDict, ensure_ascii=False, indent = 4)
+    js = json.dumps(originDeviceDict, ensure_ascii=False, indent = 4)
     filep.write(js)
     filep.close()
 
+def main():
+    deviceDict = {
+            "spark": ["晓", "晓spark"],
+            "mavic pro": ["mavic"],
+            "mavic air": [],
+            "精灵": [],
+            }
+    saveDeviceName(deviceDict)
+
+if __name__ == '__main__':
+    main()
