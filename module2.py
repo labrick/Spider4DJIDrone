f82f4c02eeb086860605c219a04f1022a5204e26
wangfuan <130wfa@gmail.com>
2018-03-01 16:18:50 +0800

    reorgnize module2 and add module1

--- module2.py
+++ module2.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
 
-#variable passed by the first step
-linkList = [
-        "https://bbs.dji.com/thread-6833-1-1.html",
-        "https://bbs.dji.com/thread-6833-1-1.html",
-        "https://bbs.dji.com/thread-2832-1-1.html",
-        "https://bbs.dji.com/thread-2832-1-1.html",
-        "https://bbs.dji.com/thread-2832-1-1.html",
-    ]
-link2DeviceList = ["mavic pro", "mavic air", "","spark", "spark"]
+from module1 import linkList, link2DeviceList
 
-#variable to be passed to next step
-linkListUnident =[]
-
-#get djiDevice
-djiDevice = list(set(link2DeviceList))
-djiDevice.remove("")
-
-#reset 
+linkListUnident = []
 djiDevicePopularity = {}
-for element in djiDevice:
-    djiDevicePopularity[element] = 0;
+djiDevice = []
 
-#calculate the popularity for now
-for index in range(len(link2DeviceList)):
-    if link2DeviceList[index] == "":
-        linkListUnident.append(linkList[index])
-    else:
-        djiDevicePopularity[link2DeviceList[index]] += 1;
+def getUnidentLinkList(linkList, link2DeviceList):
+    linkListUnident =[]
 
-print(linkListUnident)
-print(djiDevicePopularity)
-print(djiDevice)
+    djiDevice = list(set(link2DeviceList))
+    djiDevice.remove("")
+
+    djiDevicePopularity = {}
+    for element in djiDevice:
+        djiDevicePopularity[element] = 0;
+
+    for index in range(len(link2DeviceList)):
+        if link2DeviceList[index] == "":
+            linkListUnident.append(linkList[index])
+        else:
+            djiDevicePopularity[link2DeviceList[index]] += 1
+
+    return linkListUnident, djiDevicePopularity, djiDevice
+
+def main():
+    linkListUnident,djiDevicePopularity, djiDevice = getUnidentLinkList(linkList, link2DeviceList)
+    print(linkListUnident)
+    print(djiDevicePopularity)
+    print(djiDevice)
+
+if __name__ == '__main__':
+    main()
+
