41f895dd445fe7044ca381c507b1651800c9e1c6
NeoZhong <36915361+NeoZhong@users.noreply.github.com>
2018-03-09 21:00:03 +0800

    Delete getLinkDeviceList()

--- getLinkDeviceList.py
+++ getLinkDeviceList.py
@@ -5,6 +5,9 @@ import requests
 import re
 from lxml import etree
 
+BASE_URL = 'https://bbs.dji.com/'
+POST_URL = 'https://bbs.dji.com/forum-60-1.html'
+
 linkList = []
 link2DeviceList = []
 
@@ -14,19 +17,22 @@ def getHtml(html):
     return etree.HTML(page.lower())
 
 # Get the maximum number of pages
-def getMaxPageNum(html):
-    pageGet = getHtml(html)
+def getMaxPageNum():
+    pageGet = getHtml(POST_URL)
     numList = pageGet.xpath('//span[@id = "fd_page_bottom"]/div/label/span/text()')
     maxNum = int((re.findall(r"\d.*\d", numList[0]))[0])
     #print(maxNum)
     return maxNum
 
-def getLinkDeviceList(minPageNum, maxPageNum):
-    pageNum = minPageNum
+def getLinkDevice(maxPage = None):
+    if maxPage is None:
+        maxPageNum = getMaxPageNum()
+    else:
+        maxPageNum = maxPage
+    pageNum = 1
     while pageNum <= maxPageNum:
-        pageUrl = 'https://bbs.dji.com/' + 'forum-60-' + str(pageNum) + '.html'
+        pageUrl = BASE_URL + 'forum-60-' + str(pageNum) + '.html'
         sys.stdout.write("\ranalyse page:" + str(pageNum) + "/" + str(maxPageNum))
-        # print(pageUrl)
         html = getHtml(pageUrl)
         linkList.extend(html.xpath('//tbody/tr/th/p[1]/a[1]/@href'))
         link2DeviceList.extend(html.xpath('//tbody/tr/th/p[2]/em[2]/text()[1]'))
@@ -34,23 +40,13 @@ def getLinkDeviceList(minPageNum, maxPageNum):
     print()     # print \n
 
     for index in range(len(linkList)):
-        linkList[index] = 'https://bbs.dji.com/' + linkList[index]
+        linkList[index] = BASE_URL + linkList[index]
 
     for index in range(len(link2DeviceList)):
         link2DeviceList[index] = link2DeviceList[index].replace("发表于", '')
         link2DeviceList[index] = link2DeviceList[index].replace("用户", '')
         link2DeviceList[index] = link2DeviceList[index].rstrip()
 
-    return linkList, link2DeviceList
-
-def getLinkDevice(maxPage = None):
-    baseUrl = 'https://bbs.dji.com/forum-60-1.html'    # DJI社区
-    minPageNum = 1
-    if maxPage is None:
-        maxPageNum = getMaxPageNum(baseUrl)
-    else:
-        maxPageNum = maxPage
-    linkList, link2DeviceList = getLinkDeviceList(minPageNum, maxPageNum)
     print("link num:" + str(len(linkList)) + ", device Num:" + str(len(link2DeviceList)))
     return linkList, link2DeviceList
 
