96759288aa039914cd94d243f6b9564e91eb98a6
labrick <yananback@gmail.com>
2018-03-11 10:44:41 +0800

    添加数据库

--- sqliteWrapper.py
+++ sqliteWrapper.py
@@ -2,6 +2,7 @@
 # coding=UTF-8
 
 import sys
+import time
 import http.client as httplib
 import sqlite3
 from lxml import etree
@@ -18,23 +19,38 @@ class SqliteWrapper:
         self.dbPointer = self.fundb.cursor()
         self.tableName = tableName
 
+    def fmtDate(self, dateStr):
+        t = time.strptime(dateStr, "%Y-%m-%d")
+        return time.strftime("%Y-%m-%d", t)
+
+
+    def fmtDateTime(self, dateStr):
+        t = time.strptime(dateStr, "%Y-%m-%d %H:%M")
+        return time.strftime("%Y-%m-%d %H:%M", t)
+
     # save data to sqlite db
-    def saveData(self, postDate, postBy, visitTimes, commentTimes, updateTime):
-        # print("CREATE TABLE if the TABLE not exists")
+    def saveData(self, postDate, postBy, device, postLink, visitTimes, commentTimes, updateTime):
+        postDate = self.fmtDate(postDate)
+        updateTime = self.fmtDateTime(updateTime)
         sql = "CREATE TABLE IF NOT EXISTS " + self.tableName + \
-            " (postDate VARCHAR(20), postBy VARCHAR(20), visitTimes INT, commentTimes INT, updateTime VARCHAR(20))"
+            " (postDate TEXT, postBy VARCHAR(20), device TEXT, postLink TEXT UNIQUE, visitTimes INT, commentTimes INT, updateTime VARCHAR(20))"
         self.dbPointer.execute(sql)
 
         # INSERT data
         # print("deal with: %s, tableName: %s" %(date, tableName))
         try:
             self.dbPointer.execute("INSERT INTO " + self.tableName + \
-                    " VALUES (?, ?, ?, ?, ?)", (postDate, postBy, visitTimes, commentTimes, updateTime))
+                    # " VALUES (?, ?, ?, ?, ?)", (date(postDate), postBy, visitTimes, commentTimes, datetime(updateTime)))
+                    " VALUES (?, ?, ?, ?, ?, ?, ?)", (postDate, postBy, device, postLink, visitTimes, commentTimes, updateTime))
         except:
             pass
             # 修改已经存在的值
 
         self.fundb.commit()
+
+    def getNewstDate(self):
+        self.dbPointer.execute("SELECT * FROM " + self.tableName + " ORDER BY DATE(postDate) DESC")
+        return self.dbPointer.fetchone()
 
     # dump data from db
     def dumpDb(self, tableName):
