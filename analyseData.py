13b916cac7a04072442c57f95e6acbca4378f50c
wangfuan <130wfa@gmail.com>
2018-03-14 22:37:22 +0800

    添加getResult的另一种实现方式

--- analyseData.py
+++ analyseData.py
@@ -4,7 +4,6 @@
 import common
 import sys
 import getopt
-
 from sqliteWrapper import SqliteWrapper
 
 def writeDevice2Json(djiDevice):
@@ -57,7 +56,6 @@ def getStartEndMonth(start,period):
     return startMonth, endMonth 
 
 def getLinkDevice(period, months):
-   # checkDateValidation(months)
     linkList = []
     link2DeviceList = []
     sqliteWrapper = SqliteWrapper("C222")
@@ -74,49 +72,88 @@ def getLinkDevice(period, months):
         link2DeviceList.append(tmpLink2DeviceList)
     return linkList, link2DeviceList
 
+#def getResult(period,months):
+#    djiDevicePopularity = getPopularity(period, months)
+#    num = len(djiDevicePopularity)
+#    valueList = []
+#    for i in range(num):
+#        # init values
+#        nameList = []
+#        tmpValueList = []
+#        deviceSeries = {'Osmo':0, 'Osmo Mobile':0, 'Spark':0, 'Mavic Pro':0, 'Mavic Air':0, 'Phantom 4/4Pro':0, 'Phantom 3/3SE':0, 'Inspire':0, 'Other':0}
+#        # Make sure the initial value is zero
+#        for key in deviceSeries:
+#            deviceSeries[key] = 0
+#        # Classify the devices
+#        for key in djiDevicePopularity[i]:
+#            if key == 'osmo mobile' or key == 'osmo mobile 2':
+#                deviceSeries['Osmo Mobile'] += djiDevicePopularity[i][key]
+#            elif key == 'osmo' or key == 'osmo＋' or key == 'osmo pro' or key == 'osmo raw':
+#                deviceSeries['Osmo'] += djiDevicePopularity[i][key]
+#            elif key == '晓spark':
+#                deviceSeries['Spark'] += djiDevicePopularity[i][key]
+#            elif key == 'mavic pro' or key == 'mavic pro 铂金版':
+#                deviceSeries['Mavic Pro'] += djiDevicePopularity[i][key]
+#            elif key == 'mavic air':
+#                deviceSeries['Mavic Air'] += djiDevicePopularity[i][key]
+#            elif key == '精灵4a' or key == '精灵4 pro' or key == '精灵4':
+#                deviceSeries['Phantom 4/4Pro'] += djiDevicePopularity[i][key]
+#            elif key == '精灵3' or key == '精灵3 se':
+#                deviceSeries['Phantom 3/3SE'] += djiDevicePopularity[i][key]
+#            elif key == '悟1' or key == '悟2' or key == '悟raw' or key == '悟pro':
+#                deviceSeries['Inspire'] += djiDevicePopularity[i][key]
+#            else:
+#                deviceSeries['Other'] += djiDevicePopularity[i][key];
+#        deviceSerieSorted = sorted(deviceSeries.items(), key = lambda deviceSeries:deviceSeries[0])
+#        for index in deviceSerieSorted:
+#            #if index[1] == 0:
+#            #    continue
+#            nameList.append(index[0])
+#            tmpValueList.append(index[1])
+#        valueList.append(tmpValueList)
+#    return nameList, valueList 
+
 def getResult(period,months):
     djiDevicePopularity = getPopularity(period, months)
     num = len(djiDevicePopularity)
-    valueList = []
+    valueList = [] 
+    nameList = []
+    deviceSeries = {}
+    deviceDict = common.getJsonContent("deviceDict.json")
+    for seriesName,seriesList in deviceDict.items():
+        deviceSeries[seriesName] = 0
+    deviceSerieSorted = sorted(deviceSeries.items(), key = lambda deviceSeries:deviceSeries[0]);
+    for index in deviceSerieSorted:
+        nameList.append(index[0]);
     for i in range(num):
-        # init values
-        nameList = []
         tmpValueList = []
-        deviceSeries = {'Osmo':0, 'Osmo Mobile':0, 'Spark':0, 'Mavic Pro':0, 'Mavic Air':0, 'Phantom 4/4Pro':0, 'Phantom 3/3SE':0, 'Inspire':0, 'Other':0}
         # Make sure the initial value is zero
         for key in deviceSeries:
             deviceSeries[key] = 0
         # Classify the devices
         for key in djiDevicePopularity[i]:
-            if key == 'osmo mobile' or key == 'osmo mobile 2':
-                deviceSeries['Osmo Mobile'] += djiDevicePopularity[i][key]
-            elif key == 'osmo' or key == 'osmo＋' or key == 'osmo pro' or key == 'osmo raw':
-                deviceSeries['Osmo'] += djiDevicePopularity[i][key]
-            elif key == '晓spark':
-                deviceSeries['Spark'] += djiDevicePopularity[i][key]
-            elif key == 'mavic pro' or key == 'mavic pro 铂金版':
-                deviceSeries['Mavic Pro'] += djiDevicePopularity[i][key]
-            elif key == 'mavic air':
-                deviceSeries['Mavic Air'] += djiDevicePopularity[i][key]
-            elif key == '精灵4a' or key == '精灵4 pro' or key == '精灵4':
-                deviceSeries['Phantom 4/4Pro'] += djiDevicePopularity[i][key]
-            elif key == '精灵3' or key == '精灵3 se':
-                deviceSeries['Phantom 3/3SE'] += djiDevicePopularity[i][key]
-            elif key == '悟1' or key == '悟2' or key == '悟raw' or key == '悟pro':
-                deviceSeries['Inspire'] += djiDevicePopularity[i][key]
-            else:
-                deviceSeries['Other'] += djiDevicePopularity[i][key];
+            for seriesName,seriesList in deviceDict.items():
+                if isInside(seriesList,key) == True:
+                    deviceSeries[seriesName] += djiDevicePopularity[i][key]
+                    break 
         deviceSerieSorted = sorted(deviceSeries.items(), key = lambda deviceSeries:deviceSeries[0]);
         for index in deviceSerieSorted:
-            #if index[1] == 0:
-            #    continue
-            nameList.append(index[0]);
             tmpValueList.append(index[1])
         valueList.append(tmpValueList)
     return nameList, valueList 
 
+def isInside(theList, theItem):
+    ret = False 
+    for i in theList:
+        if i == theItem:
+            ret = True 
+            break 
+    return ret 
+
 def main(argv):
-    name, value = getResult(argv)
+    period = 2
+    months = ['201708','201710','201712']
+    name, value = getResult(period, months)
     print(len(name))
 
 if __name__ == "__main__":
